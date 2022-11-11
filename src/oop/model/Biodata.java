ac089157895ce74cf5046418feb384fa6a5c4582
badiuszaman <badiuszaman28@gmail.com>
2022-11-11 10:21:02 +0700

    pertama upload

---
+++ src/oop/model/Biodata.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oop.model;
+
+/**
+ *
+ * @author senja
+ */
+public class Biodata {
+    String nama, alamat, jk, jurusan, kelas;
+    public Biodata(String nama, String kelas, String jk, String alamat,  String jurusan) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.jk = jk;
+        this.jurusan = jurusan;
+        this.kelas = kelas;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getJk() {
+        return jk;
+    }
+
+    public void setJk(String jk) {
+        this.jk = jk;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+    
+}
